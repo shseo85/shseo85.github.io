685c6bab12d49b58db09c96a31fa3f8d0afa8841
shseo85 <101347769+shseo85@users.noreply.github.com>
2026-02-22 09:58:19 +0000

    Deploying to gh-pages from @ shseo85/shseo85.github.io@f76ed084afde17aa5e0be9dd88c668df8ff485e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "post-a-post-with-plotly-js",
+  },{id: "nav-publications",
+          title: "publications",
+          description: "Full publication list on Google Scholar",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
