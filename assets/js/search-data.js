8a6c4a85de69e2a89d032f054cc479afff75e388
shseo85 <101347769+shseo85@users.noreply.github.com>
2026-02-22 11:11:46 +0000

    Deploying to gh-pages from @ shseo85/shseo85.github.io@12b7cad59d598dfe1007993b3dd70fa45076f2d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "Full publication list on Google Scholar",
+          description: "Full publication list on  Google Scholar →",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
